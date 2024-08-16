3dde8f1a5039cbee732a32fa48bb4b642b430a03
LabyrinthWarden <labyrinthwarden13@gmail.com>
2024-08-16 15:34:23 -0700

    initial menu page

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -30,7 +30,7 @@ declare module '$env/static/private' {
 	export const ALLUSERSPROFILE: string;
 	export const APPDATA: string;
 	export const ChocolateyInstall: string;
-	export const ChocolateyLastPathUpdate: string;
+	export const CHROME_CRASHPAD_PIPE_NAME: string;
 	export const COLOR: string;
 	export const COLORTERM: string;
 	export const COMMONPROGRAMFILES: string;
@@ -41,7 +41,7 @@ declare module '$env/static/private' {
 	export const DISPLAY: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_9652: string;
+	export const EFC_10656: string;
 	export const EXEPATH: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
@@ -85,8 +85,8 @@ declare module '$env/static/private' {
 	export const npm_package_name: string;
 	export const npm_package_version: string;
 	export const NUMBER_OF_PROCESSORS: string;
+	export const OLDPWD: string;
 	export const OneDrive: string;
-	export const OneDriveConsumer: string;
 	export const ORIGINAL_PATH: string;
 	export const ORIGINAL_TEMP: string;
 	export const ORIGINAL_TMP: string;
@@ -94,6 +94,7 @@ declare module '$env/static/private' {
 	export const OS: string;
 	export const PATH: string;
 	export const PATHEXT: string;
+	export const PDFFOLDER: string;
 	export const PKG_CONFIG_PATH: string;
 	export const PKG_CONFIG_SYSTEM_INCLUDE_PATH: string;
 	export const PKG_CONFIG_SYSTEM_LIBRARY_PATH: string;
@@ -121,10 +122,12 @@ declare module '$env/static/private' {
 	export const TERM_PROGRAM_VERSION: string;
 	export const TMP: string;
 	export const TMPDIR: string;
+	export const USERDNSDOMAIN: string;
 	export const USERDOMAIN: string;
 	export const USERDOMAIN_ROAMINGPROFILE: string;
 	export const USERNAME: string;
 	export const USERPROFILE: string;
+	export const VBOX_MSI_INSTALL_PATH: string;
 	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
@@ -166,7 +169,7 @@ declare module '$env/dynamic/private' {
 		ALLUSERSPROFILE: string;
 		APPDATA: string;
 		ChocolateyInstall: string;
-		ChocolateyLastPathUpdate: string;
+		CHROME_CRASHPAD_PIPE_NAME: string;
 		COLOR: string;
 		COLORTERM: string;
 		COMMONPROGRAMFILES: string;
@@ -177,7 +180,7 @@ declare module '$env/dynamic/private' {
 		DISPLAY: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_9652: string;
+		EFC_10656: string;
 		EXEPATH: string;
 		GIT_ASKPASS: string;
 		HOME: string;
@@ -221,8 +224,8 @@ declare module '$env/dynamic/private' {
 		npm_package_name: string;
 		npm_package_version: string;
 		NUMBER_OF_PROCESSORS: string;
+		OLDPWD: string;
 		OneDrive: string;
-		OneDriveConsumer: string;
 		ORIGINAL_PATH: string;
 		ORIGINAL_TEMP: string;
 		ORIGINAL_TMP: string;
@@ -230,6 +233,7 @@ declare module '$env/dynamic/private' {
 		OS: string;
 		PATH: string;
 		PATHEXT: string;
+		PDFFOLDER: string;
 		PKG_CONFIG_PATH: string;
 		PKG_CONFIG_SYSTEM_INCLUDE_PATH: string;
 		PKG_CONFIG_SYSTEM_LIBRARY_PATH: string;
@@ -257,10 +261,12 @@ declare module '$env/dynamic/private' {
 		TERM_PROGRAM_VERSION: string;
 		TMP: string;
 		TMPDIR: string;
+		USERDNSDOMAIN: string;
 		USERDOMAIN: string;
 		USERDOMAIN_ROAMINGPROFILE: string;
 		USERNAME: string;
 		USERPROFILE: string;
+		VBOX_MSI_INSTALL_PATH: string;
 		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 		VSCODE_GIT_ASKPASS_MAIN: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
--- src/routes/menu/+page.js
+++ src/routes/menu/+page.js
@@ -1,4 +1,72 @@
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-    return {};
+    const menu = [
+      {
+        category: 'Three Egg Omelettes',
+        category_note:
+          'Served with hash browns or home fries and a choice of toast or pancakes',
+        dishes: [
+          {
+            name: 'Express',
+            description:
+              'Italian sausage, mushrooms, tomatoes, spinach, Jack cheese, and a homemade salsa',
+            price: 16.0,
+          },
+          {
+            name: 'Carne Asada',
+            description:
+              'Grilled top sirloin steak, mushrooms, onions, bell peppers, Jack cheese, a side of pico, and topped with guacamole',
+            price: 16.0,
+          },
+          {
+            name: 'Vegetarian',
+            description: 'Grilled fresh vegetables with your choice of cheese',
+            price: 15.0,
+          },
+          {
+            name: 'Denver',
+            description: 'Ham, onions, bell peppers, and cheddar cheese',
+            price: 15.5,
+          },
+          {
+            name: 'California',
+            description:
+              'Bacon, tomatoes, cheddar cheese, and topped with avocados',
+            price: 15.5,
+          },
+          {
+            name: 'Seafood',
+            description:
+              'A mix of sauteed shrimp, scallops, crab, and fish, Jack cheese, and topped with our creamy Hollandaise sauce',
+            price: 16.0,
+          },
+          {
+            name: 'Spinach',
+            description: 'Fresh spinach, mushrooms, and cream cheese',
+            price: 15.5,
+          },
+          {
+            name: 'Turkey',
+            description:
+              'Pulled turkey, tomatoes, mushrooms, Jack cheese, and topped with our creamy Hollandaise sauce',
+            price: 16.0,
+          },
+          {
+            name: 'Chili Bean',
+            description:
+              'Our award-winning chili beans, topped with cheddar chees and onions',
+            price: 15.5,
+          },
+          {
+            name: 'Ham, Bacon, or Sausage',
+            description: 'With choice of cheese and onions',
+            price: 14.5,
+          },
+        ],
+      }
+    ]
+    
+    return {
+        menu
+    };
 };
