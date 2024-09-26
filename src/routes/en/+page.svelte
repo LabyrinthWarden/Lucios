313ae773c60b73a451f491574d67254fd5803345
LabyrinthWarden <labyrinthwarden13@gmail.com>
2024-09-26 15:38:08 -0700

    prettier reformat

--- src/routes/en/+page.svelte
+++ src/routes/en/+page.svelte
@@ -1,25 +1,28 @@
 <script>
-    import heroImage from '$lib/assets/img/hero.jpg'
-      import { toast } from '@zerodevx/svelte-toast'
+  import heroImage from '$lib/assets/img/hero.jpg'
+  import { toast } from '@zerodevx/svelte-toast'
   import { onMount } from 'svelte'
 
   onMount(() => {
-    toast.push("Lucio's will be closed Friday, September 20th through Monday, September 23rd", {
-      // Effectively disables autoclose when `initial`==`next`
-      initial: 0,
-    })
+    toast.push(
+      "Lucio's will be closed Friday, September 20th through Monday, September 23rd",
+      {
+        // Effectively disables autoclose when `initial`==`next`
+        initial: 0,
+      }
+    )
   })
 </script>
 
-<div
-  class="hero min-h-screen"
-  style="background-image: url({heroImage});">
+<div class="hero min-h-screen" style="background-image: url({heroImage});">
   <div class="hero-overlay bg-opacity-60"></div>
   <div class="hero-content text-neutral-content text-center">
     <div class="max-w-md">
       <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
       <p class="mb-5">
-        We're a small family owned business with delicious Mexican, Chinese, and American food. We serve breakfast, lunch, and dinner all day, Tuesday-Sunday.
+        We're a small family owned business with delicious Mexican, Chinese, and
+        American food. We serve breakfast, lunch, and dinner all day,
+        Tuesday-Sunday.
       </p>
       <a href="/menu"><button class="btn btn-success">See Our Menu</button></a>
     </div>
