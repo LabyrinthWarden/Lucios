ae47e4250148ef012d87863f1be5a629cf254418
LabyrinthWarden <labyrinthwarden13@gmail.com>
2024-09-26 16:42:05 -0700

    updated menu

--- src/routes/en/menu/+page.svelte
+++ src/routes/en/menu/+page.svelte
@@ -4,14 +4,46 @@
   console.log(data)
 </script>
 
-<div class="grid grid-cols-1 place-items-center mt-10 w-full">
+<div class="grid grid-cols-1 place-items-center mt-10 mb-5 w-full">
   <h2 class="text-6xl title-text">Menu & Pricing</h2>
 </div>
 
-<div>
-  {#each data.menu as item}
-    <ul>
-      <li>{item.cetegory}</li>
-    </ul>
-  {/each}
+<div class="carousel w-full">
+  <div id="page1" class="carousel-item relative w-full">
+    <div class="grid grid-cols-2 w-full">
+      <h3 class="col-span-full mx-auto text-3xl text-warning">Three Egg Omelette</h3>
+    </div>
+    
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#page4" class="btn btn-circle">❮</a>
+      <a href="#page2" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="page2" class="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#page1" class="btn btn-circle">❮</a>
+      <a href="#page3" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="page3" class="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#page2" class="btn btn-circle">❮</a>
+      <a href="#page4" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="page4" class="carousel-item relative w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#page3" class="btn btn-circle">❮</a>
+      <a href="#page1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
 </div>
