e679407b22043a1ddeb8f7863c041a63d863c5fc
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-09-27 11:47:34 -0700

    updates for menu

--- src/routes/en/menu/+page.svelte
+++ src/routes/en/menu/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   /** @type {import('./$types').PageData} */
   export let data
-  console.log(data)
 </script>
 
 <div class="grid grid-cols-1 place-items-center mt-10 mb-5 w-full">
@@ -10,20 +9,35 @@
 
 <div class="carousel w-full">
   <div id="page1" class="carousel-item relative w-full">
-    <div class="grid grid-cols-2 w-full">
-      <h3 class="col-span-full mx-auto text-3xl text-warning">Three Egg Omelette</h3>
-    </div>
-    
-    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#page4" class="btn btn-circle">❮</a>
-      <a href="#page2" class="btn btn-circle">❯</a>
+    <div class='w-full'>
+      <div class="grid grid-cols-1 w-full border">
+        <h3
+          class="w-full mx-auto text-3xl text-warning text-semibold small-caps"
+        >
+          Three Egg Omelette
+        </h3>
+        <p class="w-full mx-auto">
+          Served with hash browns or home fries and a choice of toast or
+          pancakes
+        </p>
+      </div>
+
+      <div
+        class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+      >
+        <a href="#page4" class="btn btn-circle">❮</a>
+        <a href="#page2" class="btn btn-circle">❯</a>
+      </div>
     </div>
   </div>
   <div id="page2" class="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      class="w-full" />
-    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      class="w-full"
+    />
+    <div
+      class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+    >
       <a href="#page1" class="btn btn-circle">❮</a>
       <a href="#page3" class="btn btn-circle">❯</a>
     </div>
@@ -31,8 +45,11 @@
   <div id="page3" class="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      class="w-full" />
-    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      class="w-full"
+    />
+    <div
+      class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+    >
       <a href="#page2" class="btn btn-circle">❮</a>
       <a href="#page4" class="btn btn-circle">❯</a>
     </div>
@@ -40,8 +57,11 @@
   <div id="page4" class="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      class="w-full" />
-    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      class="w-full"
+    />
+    <div
+      class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+    >
       <a href="#page3" class="btn btn-circle">❮</a>
       <a href="#page1" class="btn btn-circle">❯</a>
     </div>
