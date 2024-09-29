83206f25261de85a0af3a244a4d07140075357e3
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-09-29 16:28:09 -0700

    started menu page 2

--- src/routes/en/menu/+page.svelte
+++ src/routes/en/menu/+page.svelte
@@ -264,10 +264,142 @@
     </div>
   </div>
   <div id="page2" class="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      class="w-full"
-    />
+    <div class="w-full">
+      <!-- Sandwiches Section -->
+      <MenuGroup
+        category="Sandwiches"
+        categoryDescription="Served with coleslaw, salad, fesh cut fries, or curly fries."
+      >
+      <MenuItem 
+        name="Grilled Cheese"
+        price="10.95"
+        description="Three pieces of whitew bread with American cheese."
+      />
+      <MenuItem 
+        name="Monte Cristo"
+        price="14.95"
+        description="Ham, Swiss cheese, bacon, and tomatoes on Texas French Toast."
+      />
+      <MenuItem 
+        name="Grilled Cheese and Tomatoes"
+        price="12.95"
+        description="Grilled tomatoes and American cheese on sourdough bread."
+      />
+      <MenuItem 
+        name="Philly Cheese Steak"
+        price="15.95"
+        description="Slices of roast beef, Swiss cheese, grilled onions, and bell peppers on a French roll"
+      />
+      <MenuItem 
+        name="Grilled Ham and Cheese"
+        price="12.95"
+        description="Ham and American cheese on sourdough bread."
+      />
+      <MenuItem 
+        name="New York Steak"
+        price="20.95"
+        description="New York Steak with grilled onions, lettuce, and tomatoes on a steak roll."
+      />
+      <MenuItem 
+        name="Chicken Salad Sandwich"
+        price="12.95"
+        description="Chicken salad with lettuce on a French roll."
+      />
+      <MenuItem 
+        name="Tuna Melt"
+        price="13.95"
+        description="Tuna salad with American cheese on grilled sourdough."
+      />
+      <MenuItem 
+        name="Tuna Salad Sandwich"
+        price="12.95"
+        description="Tuna salad with lettuce on white toast."
+      />
+      <MenuItem 
+        name="Patty Melt"
+        price="15.95"
+        description="All beef patty, American cheese, and grilled onions on grilled rye."
+      />
+      <MenuItem 
+        name="Grilled Club"
+        price="15.95"
+        description="Ham, roast beef, roasted turkey, bacon, tomatoes, and American cheese on grilled sourdough bread."
+      />
+      <MenuItem 
+        name="Bagel"
+        price="14.95"
+        description="An everything bagel with one egg any style, American cheese, tomatoes, and onions. Choice of sausage patty, ham, or bacon."
+      />
+      <MenuItem 
+        name="Triple Decker Club"
+        price="15.95"
+        description="Ham, roasted turkey, bacon, and American cheese toasted white bread."
+      />
+      <MenuItem 
+        name="Turkey Melt"
+        price="14.95"
+        description="Turkey, grilled onions, and tomatoes with Jack cheese and avacadoes on grilled rye bread."
+      />
+      <MenuItem 
+        name="BLT"
+        price="13.95"
+        description="Bacon, lettuce, and tomatoes on white bread."
+      />
+      </MenuGroup>
+      <MenuGroup
+        category="Burgers"
+        categoryDescription="1/3 lb. all beef patty served with salad, fresh cut fries, or curly fries."
+      >
+        <MenuItem 
+        name="Bacon Cheeseburger"
+        price="15.95"
+      />
+      <MenuItem 
+        name="Western Burger"
+        price="16.95"
+        description="Pepper Jack cheese, bacon, and BBQ sauce, topped with an onion ring."
+      />
+      <MenuItem 
+        name="Double Cheeseburger"
+        price="19.99"
+        description="Add bacon 2.00"
+      />
+      <MenuItem 
+        name="Fish Burger"
+        price="14.95"
+        description="Fried tilapia topped with lettuce, tomatoes, and onions."
+      />
+      <MenuItem 
+        name="Hamburger"
+        price="12.95"
+        description="Add cheese 1.00"
+      />
+      <MenuItem 
+        name="Stuffed Burger"
+        price="16.95"
+        description="Stuffed with bacon, mushrooms, and Swiss cheese."
+      />
+      <MenuItem 
+        name="Teriyaki Burger"
+        price="15.95"
+      />
+      <MenuItem 
+        name="Pulled Pork Burger"
+        price="14.95"
+        description="BBQ pulled pork topped with onions rings and coleslaw."
+      />
+      <MenuItem 
+        name="Chili Burger"
+        price="15.95"
+        description="Homemade chili topped with cheddar cheese and onions."
+      />
+      <MenuItem 
+        name="Teriyaki Chicken Burger"
+        price="14.95"
+        description="Chicken breast topped with grilled onions and mushrooms."
+      />
+      </MenuGroup>
+    </div>
     <div
       class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
     >
