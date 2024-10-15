9a8bdf539dc0037deb0a97610bde2bfd10914386
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-10-15 14:18:47 -0700

    removed old language script

--- src/i18n.js
+++
@@ -1,9 +0,0 @@
-import { init, register, getLocaleFromNavigator } from 'svelte-i18n'
-
-register('en', () => import('./locales/en.json'))
-register('es', () => import('./locales/es.json'))
-
-init({
-  fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
-})
